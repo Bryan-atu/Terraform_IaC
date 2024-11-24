fc79535ceb9ee629b88522a737d42132c88660fd
Bryan-atu <L001885441@atu.ie>
2024-11-24 23:48:48 +0000

    [feat/terra3] Refactor code to be more modular

--- main.py
+++ main.py
@@ -4,45 +4,65 @@ from cdktf import App, NamedRemoteWorkspace, TerraformStack, TerraformOutput, Re
 from cdktf_cdktf_provider_aws.provider import AwsProvider
 from cdktf_cdktf_provider_aws.instance import Instance
 
+class AwsVariables:
+    def __init__(self, stack: TerraformStack):
+        # Define Terraform variables for region and AMI ID
+        self.region_var = TerraformVariable(stack, "region", default="eu-west-1", description="AWS region to deploy resources")
+        self.ami_var = TerraformVariable(stack, "ami_id", default="ami-008d05461f83df5b1", description="AMI ID to use for the EC2 instance")
+
+    def get_region(self):
+        return self.region_var  # Return the variable references
+
+    def get_ami(self):
+        return self.ami_var  
+
+class AwsProviderConfig:
+    def __init__(self, stack: TerraformStack, region: TerraformVariable):
+        AwsProvider(stack, "AWS", region=region.to_string())
+
+class Ec2Instance:
+    def __init__(self, stack: TerraformStack, ami: TerraformVariable, instance_type: str = "t2.micro", tags: dict = None):
+        # Create the EC2 instance using the ami variable
+        self.instance = Instance(stack, "compute",
+                                  ami=ami.to_string(),
+                                  instance_type=instance_type,
+                                  tags=tags or {
+                                      "Name": "TF2",
+                                      "Environment": "Development",
+                                      "Owner": "Dev Team"
+                                  })
+
+    def get_instance(self):
+        return self.instance
+
+class Outputs:
+    def __init__(self, stack: TerraformStack, instance: Instance):
+        TerraformOutput(stack, "public_ip", value=instance.public_ip)
+        TerraformOutput(stack, "instance_id", value=instance.id)
+
 class MyStack(TerraformStack):
     def __init__(self, scope: Construct, ns: str):
         super().__init__(scope, ns)
 
-        # Define a variable for the region
-        region_var = TerraformVariable(self, "region", default="eu-west-1", description="AWS region to deploy resources")
-
-        # Define a variable for the AMI ID
-        ami_var = TerraformVariable(self, "ami_id", 
-                                    default="ami-008d05461f83df5b1", 
-                                    description="AMI ID to use for the EC2 instance")
+        # Initialize variables
+        aws_vars = AwsVariables(self)
         
+        # Initialize AWS provider with the region variable
+        AwsProviderConfig(self, aws_vars.get_region()) 
 
-        AwsProvider(self, "AWS", region=region_var.value)
+        # Create EC2 instance
+        instance = Ec2Instance(self, aws_vars.get_ami())
 
-
-        # Define EC2 instance
-        instance = Instance(self, "compute",
-                            ami=ami_var.to_string(),
-                            instance_type="t2.micro",
-                            tags={
-                                "Name": "TF2",
-                                "Environment": "Development",
-                                "Owner": "Dev Team"
-                            })
-
-        # Output IPs
-        TerraformOutput(self, "public_ip", value=instance.public_ip)
-
-        TerraformOutput(self, "instance_id", value=instance.id)
+        # Create outputs
+        Outputs(self, instance.get_instance())
 
 app = App()
 stack = MyStack(app, "aws_instance")
 
+# Configure remote backend
 RemoteBackend(stack,
               hostname='app.terraform.io',
               organization='IaC_Terraform_BG',
-              workspaces=NamedRemoteWorkspace('IaC_Tf2')
-              )
+              workspaces=NamedRemoteWorkspace('IaC_Tf2'))
 
-# Synthesize the application
 app.synth()
